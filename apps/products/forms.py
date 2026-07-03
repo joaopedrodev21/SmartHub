b649b6dc1ca974088c9b102bf60fa9ee57213696
João Pedro <joaopedrobarbosadasilva021@gmail.com>
2026-07-03 17:33:26 -0300

    feat: Organized the products environment with views, forms, templates, and admin panel tests.

---
+++ apps/products/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name', 'description', 'price', 'stock', 'company']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+            'company': forms.Select(attrs={'class': 'form-control'}),
+        }
--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from .models import Product
+from .forms import ProductForm
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/list.html'
+    context_object_name = 'products'
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    template_name = 'products/form.html'
+    success_url = reverse_lazy('products:product_list')
+
+class ProductUpdateView(UpdateView):
+    model = Product 
+    form_class = ProductForm
+    template_name = 'products/form.html'
+    success_url = reverse_lazy('products:product_list')
+
+class ProductDeleteView(DeleteView):
+    model = Product 
+    template_name = 'products/confirm_delete.html'
+    success_url = reverse_lazy('products:product_list')
